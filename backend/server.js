50e40e5014cbb70e11cdc0dcd0056c3dcdc428c6
Anurag Amchi <anuragamchi@gmail.com>
2025-12-02 18:37:15 +0530

    package in backend

--- backend/server.js
+++ backend/server.js
@@ -18,18 +18,6 @@ app.get('/', (req, res) => {
     res.send("API is running");
 })
 
-// app.get('/api/chat', (req, res) => {
-//     res.send(chats)
-// })
-
-// app.get('/api/chat/:id', (req, res) => {
-//     // console.log(req.params.id);
-
-//     const singleChat = chats.find((c) => c._id === req.params.id);
-//     res.send(singleChat);
-
-// })
-
 app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
 app.use('/api/message', messageRoutes)
